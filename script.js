7d68211a145aa1ab71f1505257c71dac2f553b21
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2023-06-11 11:41:27 +0200

    set initial variables

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const equalsBtn = document.querySelector('[data-equals')
 const deleteBtn = document.querySelector('[data-delete]')
 const allClearBtn = document.querySelector('[data-all-clear')
 
+let previousOperand = ""
+let currentOperand = ""
+let operation = ""
+let result
+
+
 function add (a, b) {
     return a + b
 }
