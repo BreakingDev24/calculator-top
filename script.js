2f52fd395077c42a4f605d4c85d677cbe5c0249a
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2023-06-11 11:36:56 +0200

    set document variable

---
+++ script.js
@@ -0,0 +1,6 @@
+const previousOperandDisplay = document.getElementById('previous-operand');
+const numberBtn = document.querySelectorAll('[data-number]')
+const operationBtn = document.querySelectorAll('[data-operation')
+const equalsBtn = document.querySelector('[data-equals')
+const deleteBtn = document.querySelector('[data-delete]')
+const allClearBtn = document.querySelector('[data-all-clear')
