358195df53eb4205c63305a8711afefc0c5786bd
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2023-06-11 11:39:35 +0200

    create operation function

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const operationBtn = document.querySelectorAll('[data-operation')
 const equalsBtn = document.querySelector('[data-equals')
 const deleteBtn = document.querySelector('[data-delete]')
 const allClearBtn = document.querySelector('[data-all-clear')
+
+function add (a, b) {
+    return a + b
+}
+
+function subtraction (a, b) {
+    return a - b
+}
+
+function multiplication (a, b) {
+    return a * b
+}
+
+function division (a, b) {
+    return a / b
+}
